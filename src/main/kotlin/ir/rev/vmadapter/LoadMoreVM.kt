a7987865d7abe9ae99fe84a31c52cbe9f6bf1d0d
au.pervov <au.pervov@tensor.ru>
2022-12-20 21:55:23 +0700

    save

--- src/main/kotlin/ir/rev/vmadapter/LoadMoreVM.kt
+++
@@ -1,9 +0,0 @@
-package ir.rev.vmadapter
-
-import androidx.databinding.BaseObservable
-
-/**@SelfDocumented*/
-data class LoadMoreVM(
-    val indeterminate: Boolean = true,
-    val delayedShowing: Boolean = false,
-) : BaseObservable()
